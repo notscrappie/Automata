e6b93a84648306c3aa33fa4b1ae797dc8de02d90
scrappie <scr3ppie@gmail.com>
2023-02-24 21:26:13 +0200

    update index

--- index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	Node: require('./src/Node'),
-	Player: require('./src/Player'),
-	Manager: require('./src/Manager'),
-	Queue: require('./src/guild/Queue'),
-	Filter: require('./src/guild/Filter'),
-	Track: require('./src/guild/Track'),
-	PoruTrack: require('./src/guild/PoruTrack'),
-};
---
+++ src/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+	Node: require('./classes/Node'),
+	Player: require('./classes/Player'),
+	Manager: require('./classes/Manager'),
+	Queue: require('./classes/Queue'),
+	Track: require('./classes/Track'),
+	AutomataTrack: require('./classes/PoruTrack'),
+};
