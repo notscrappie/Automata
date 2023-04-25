85015dc1c743b123be1216e47111df486b9185f5
scrappie <scr3ppie@gmail.com>
2023-04-25 16:42:16 +0300

    refactor: queue improvements
    - Made all functions public.
    - Modified type from Array to Array<Track>.
    - Added current and previous track to make it easier to access them.

--- src/Guild/Queue.ts
+++ src/Guild/Queue.ts
@@ -1,39 +1,44 @@
-import { Track } from "./Track"
+import { Track } from './Track';
 
-export default class Queue extends Array {
-    /** Gets the size of the queue. */
-    get size(): number {
-      return this.length;
-    }
-  
-    /** Gets the first song in the queue. */
-    first() {
-      return this ? this[0] : 0;
-    }
-  
-    /** Adds a new track to the queue. */
-    add(track: Track): Queue {
-      this.push(track);
-      return this;
-    }
-  
-    /** Removes a track from the queue. */
-    remove(index: number) {
-      return this.splice(index, 1)[0];
-    }
-  
-    /** Clears the queue. */
-    clear() {
-      return this.splice(0);
-    }
-  
-    /** Shuffles the queue. */
-    shuffle() {
-      for (let i = this.length - 1; i > 0; i -= 1) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [this[i], this[j]] = [this[j], this[i]];
-      }
-    }
-  }
-  
-  
+export default class Queue extends Array<Track> {
+	/** Gets the size of the queue. */
+	public get size(): number {
+		return this.length;
+	}
+
+	/** Gets the first song in the queue. */
+	public first() {
+		return this ? this[0] : 0;
+	}
+
+	/** Adds a new track to the queue. */
+	public add(track: Track): Queue {
+		this.push(track);
+		return this;
+	}
+
+	/** The current track. */
+	public current: Track | null = null;
+
+	/** The previous track. */
+	public previous: Track | null = null;
+
+	/** Removes a track from the queue. */
+	public remove(index: number) {
+		return this.splice(index, 1)[0];
+	}
+
+	/** Clears the queue. */
+	public clear() {
+		return this.splice(0);
+	}
+
+	/** Shuffles the queue. */
+	public shuffle() {
+		for (let i = this.length - 1; i > 0; i -= 1) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[this[i], this[j]] = [this[j], this[i]];
+		}
+	}
+}
+
