71a099ffcb83be24bafc98af720c9c3a133b05fd
scrappie <scr3ppie@gmail.com>
2023-04-26 20:24:20 +0300

    fix: remove uploaded package

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from './src/Manager';
-export * from './src/Player/Connection';
-export * from './src/Player/Player';
-export * from './src/Node/Node';
-export * from './src/Node/Rest';
-export * from './src/Player/Filters';
-export * from './src/Guild/Track';
