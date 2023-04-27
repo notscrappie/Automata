59c81ed2f8fefb6fa9e883e47bd4ef4b1894723c
scrappie <scr3ppie@gmail.com>
2023-04-28 01:30:17 +0300

    refactor: manager improvements
    - "nodes" parameter has been merged into AutomataOptions. You'll need to pass nodes alongside resumeKey, resumeTimeout etc now.
    - Removed "client" and "nodes" from constructor parameters.
    - Removed unused this.client, this.userId and this.send properties.
    - Added a void type to <Manager>.init().
    - Made leastUsedNodes, getNode and resolve functions public.
    - Made packetUpdate private.
    - Refactored the resolve function to support the newly refactored Track class.
    - Made decodeTrack and decodeTracks promise-based functions.
    - Moved NodeOptions interface to the Node class.
    - Added a new ResolveResult interface for the resolve function.
    - LoadType is no longer exported.
    - Renamed playerStart to trackStart, playerEnd to trackEnd, playerDisconnect to queueEnd and playerError to trackStuck.
    - Added support for playerUpdate and playerDestroy events.

--- src/Manager.ts
+++ src/Manager.ts
@@ -1,9 +1,8 @@
-import { Response } from './Guild/Response';
+import { AutomataTrack, TrackData } from './Guild/Track';
+import { Node, NodeOptions } from './Node/Node';
 import { Player } from './Player/Player';
-import { Track } from './Guild/Track';
 import { EventEmitter } from 'events';
 import { Client } from 'discord.js';
-import { Node } from './Node/Node';
 
 export class Manager extends EventEmitter {
 	public readonly client: Client;
@@ -18,21 +17,18 @@ export class Manager extends EventEmitter {
 	public isActivated: boolean;
 	public send: (_: unknown) => void;
 
-	constructor(client: Client, nodes: NodeOptions[], options: AutomataOptions) {
+	constructor(options: AutomataOptions) {
 		super();
-		this.client = client;
-		this._nodes = nodes;
+		this._nodes = options.nodes;
 		this.nodes = new Map();
 		this.players = new Map();
 		this.options = options;
-		this.userId = null;
 		this.version = 'v2.1';
 		this.isActivated = false;
-		this.send = null;
 	}
 
 	/** Initializes the manager. */
-	public init(client: Client) {
+	public init(client: Client): void {
 		this.userId = client.user.id;
 		for (const node of this._nodes) this.addNode(node);
 
@@ -52,7 +48,7 @@ export class Manager extends EventEmitter {
 	public addNode({ name, host, password, port }: NodeOptions): Node {
 		const node = new Node(this, { name, host, password, port }, this.options);
 		this.nodes.set(name, node);
-		node?.connect?.();
+		node?.connect();
 		return node;
 	}
 
@@ -65,14 +61,14 @@ export class Manager extends EventEmitter {
 	}
 
 	/** Gets the least used nodes. */
-	get leastUsedNodes() {
+	public get leastUsedNodes(): Node[] {
 		return [...this.nodes.values()]
 			.filter((node) => node.isConnected)
 			.sort((a, b) => a.penalties - b.penalties);
 	}
 
 	/** Retrives a node. */
-	getNode(identifier = 'auto') {
+	public getNode(identifier = 'auto'): Node[] | Node {
 		if (!this.nodes.size) throw new Error('There aren\'t any available nodes.');
 		if (identifier === 'auto') return this.leastUsedNodes;
 
@@ -90,15 +86,15 @@ export class Manager extends EventEmitter {
 
 		const player = this.players.get(options.guildId);
 		if (player) {
-			const node = this.nodes.get(this.leastUsedNodes[0].name);
+			const node = this.nodes.get(this.leastUsedNodes[0].options.name);
 			if (!node) throw new Error('There aren\'t any nodes available.');
 		}
 
 		if (this.leastUsedNodes.length === 0) throw new Error('There aren\'t any nodes available.');
 
 		const foundNode = this.nodes.get(options.region
-			? this.leastUsedNodes.find((node) => node.regions.includes(options.region.toLowerCase()))?.name
-			: this.leastUsedNodes[0].name);
+			? this.leastUsedNodes.find((node) => node.regions.includes(options.region.toLowerCase()))?.options.name
+			: this.leastUsedNodes[0].options.name);
 
 		if (!foundNode) throw new Error('There aren\'t any nodes available.');
 
@@ -106,7 +102,7 @@ export class Manager extends EventEmitter {
 	}
 
 	/** Sends packet updates. */
-	public packetUpdate(packet: VoicePacket) {
+	private packetUpdate(packet: VoicePacket) {
 		if (!['VOICE_STATE_UPDATE', 'VOICE_SERVER_UPDATE'].includes(packet.t)) return;
 		const player = this.players.get(packet.d.guild_id);
 		if (!player) return;
@@ -126,7 +122,7 @@ export class Manager extends EventEmitter {
 	}
 
 	/** Creates a new player using the node and options provided by the create() function. */
-	private createPlayer(node: Node, options: ConnectionOptions) {
+	private createPlayer(node: Node, options: ConnectionOptions): Player {
 		const player = new Player(this, node, options);
 		this.players.set(options.guildId, player);
 		player.connect(options);
@@ -139,7 +135,7 @@ export class Manager extends EventEmitter {
 	}
 
 	/** Resolves the provided query. */
-	async resolve({ query, source, requester }: ResolveOptions, node?: Node): Promise<Response> {
+	public async resolve({ query, source, requester }: ResolveOptions, node?: Node): Promise<ResolveResult> {
 		if (!this.isActivated) throw new Error('Automata has not been initialized. Initiate Automata using the <Manager>.init() function in your ready.js.');
 
 		node = node ?? this.leastUsedNodes?.[0];
@@ -148,12 +144,19 @@ export class Manager extends EventEmitter {
 		const regex = /^https?:\/\//;
 		const identifier = regex.test(query) ? query : `${source ?? 'dzsearch'}:${query}`;
 
-		const response = await node.rest.get(`/v3/loadtracks?identifier=${encodeURIComponent(identifier)}`);
-		return new Response(response, requester);
+		const res = await node.rest.get(`/v3/loadtracks?identifier=${encodeURIComponent(identifier)}`) as LavalinkResponse;
+		const mappedTracks = res.tracks.map((track: TrackData) => new AutomataTrack(track, requester)) || [];
+		const finalResult: ResolveResult = {
+			loadType: res.loadType,
+			tracks: mappedTracks,
+			playlistInfo: res.playlistInfo || undefined,
+		};
+
+		return finalResult;
 	}
 
 	/** Sends a GET request to the Lavalink node to decode the provided track. */
-	decodeTrack(track: string, node?: Node) {
+	async decodeTrack(track: string, node?: Node): Promise<unknown> {
 		const targetNode = node ?? this.leastUsedNodes[0];
 		return targetNode.rest.get(
 			`/v3/decodetrack?encodedTrack=${encodeURIComponent(track)}`,
@@ -161,7 +164,7 @@ export class Manager extends EventEmitter {
 	}
 
 	/** Sends a POST request to the Lavalink node to decode the provided tracks. */
-	async decodeTracks(tracks: string[], node?: Node) {
+	async decodeTracks(tracks: string[], node?: Node): Promise<unknown> {
 		const targetNode = node ?? this.leastUsedNodes[0];
 		return await targetNode.rest.post('/v3/decodetracks', tracks);
 	}
@@ -184,24 +187,33 @@ export class Manager extends EventEmitter {
 	}
 }
 
-export interface NodeOptions {
-	/** Name of the node. */
+interface PlaylistInfo {
 	name: string;
-	/** IP of the node. */
-	host: string;
-	/** Port of the node. */
-	port: number;
-	/** Password of the node. */
-	password: string;
-	/** Requires to be set as true when the node has SSL enabled. Otherwise, it can be left disabled. */
-	secure?: boolean;
-	/** Allows you to set this node to be used across specific regions. */
-	region?: string[];
+	selectedTrack?: number;
 }
 
-export type SearchPlatform = 'spsearch' | 'dzsearch' | 'scsearch';
+interface ResolveResult {
+	loadType: LoadType;
+	tracks: AutomataTrack[];
+	playlistInfo?: PlaylistInfo;
+}
 
-export interface ResolveOptions {
+interface LavalinkResponse {
+	tracks: TrackData[];
+	loadType: LoadType;
+	playlistInfo?: PlaylistInfo;
+}
+
+type LoadType =
+	| 'TRACK_LOADED'
+	| 'PLAYLIST_LOADED'
+	| 'SEARCH_RESULT'
+	| 'NO_MATCHES'
+	| 'LOAD_FAILED'
+
+type SearchPlatform = 'spsearch' | 'dzsearch' | 'scsearch';
+
+interface ResolveOptions {
 	/** The query provided by the user. */
 	query: string;
 	/** The source that will be used to get the song from. */
@@ -211,6 +223,8 @@ export interface ResolveOptions {
 }
 
 export interface AutomataOptions {
+	/** The nodes the player will use. */
+	nodes: NodeOptions[];
 	/** The default platform used by the manager. Default platform is Deezer, by default. */
 	defaultPlatform?: SearchPlatform | string;
 	/** The time the manager will wait before trying to reconnect to a node. */
@@ -240,10 +254,9 @@ export interface ConnectionOptions {
 
 export interface AutomataEvents {
 	/**
-	 *
-	 * @param topic from what section the event come
+	 * @param topic
 	 * @param args
-	 * Emitted when a Response is come
+	 * Provides access to raw WS events. Can be used to handle custom or unknown events.
 	 * @eventProperty
 	 */
 	raw: (topic: string, ...args: unknown[]) => void;
@@ -276,35 +289,47 @@ export interface AutomataEvents {
 	 * Emitted when a player starts playing a new track.
 	 * @eventProperty
 	 */
-	playerStart: (player: Player, track: Track) => void;
+	trackStart: (player: Player, track: AutomataTrack) => void;
 
 	/**
 	 * Emitted when the player finishes playing a track.
 	 * @eventProperty
 	 */
-	playerEnd: (
+	trackEnd: (
 		player: Player,
-		track: Track,
+		track: AutomataTrack,
 		LavalinkData?: unknown
 	) => void;
 
 	/**
-	 * Emitted when the player disconnects from the Discord voice channel.
+	 * Emitted when the player's queue has finished.
 	 * @eventProperty
 	 */
-	playerDisconnect: (player: Player) => void;
+	queueEnd: (player: Player) => void;
 
 	/**
 	 * Emitted when a track gets stuck while it is playing.
 	 * @eventProperty
 	 */
-	playerError: (player: Player, track: Track, data: unknown) => void;
+	trackStuck: (player: Player, track: AutomataTrack, data: unknown) => void;
+
+	/**
+   	 * Emitted when the player gets updated.
+   	 * @eventProperty
+   	 */
+	playerUpdate: (player: Player) => void;
+
+	/**
+   	 * Emitted when a player gets destroyed.
+   	 * @eventProperty
+     */
+	playerDestroy: (player: Player) => void;
 
 	/**
 	 * Emitted when the connection between the WebSocket and Discord voice servers drops.
 	 * @eventProperty
 	 */
-	playerClose: (player: Player, track: Track, data: unknown) => void;
+	socketClose: (player: Player, track: AutomataTrack, data: unknown) => void;
 }
 
 export declare interface Manager {
