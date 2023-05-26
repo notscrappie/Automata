f571973933de4c713fdeeeed3aa315d56e9917c6
scrappie <scr3ppie@gmail.com>
2023-05-26 22:23:33 +0300

    refactor: added types and type docs

--- src/Guild/Queue.ts
+++ src/Guild/Queue.ts
@@ -11,7 +11,10 @@ export default class Queue extends Array<AutomataTrack> {
 		return this ? this[0] : 0;
 	}
 
-	/** Adds a new track to the queue. */
+	/**
+	 * Adds a new track to the queue.
+	 * @param track - The track that will be added to the queue.
+	 */
 	public add(track: AutomataTrack): Queue {
 		this.push(track);
 		return this;
@@ -23,18 +26,21 @@ export default class Queue extends Array<AutomataTrack> {
 	/** The previous track. */
 	public previous: AutomataTrack | null = null;
 
-	/** Removes a track from the queue. */
-	public remove(index: number) {
+	/**
+	 * Removes a track from the queue.
+	 * @param index - The number of the track that will be removed.
+	*/
+	public remove(index: number): AutomataTrack {
 		return this.splice(index, 1)[0];
 	}
 
 	/** Clears the queue. */
-	public clear() {
+	public clear(): AutomataTrack[] {
 		return this.splice(0);
 	}
 
 	/** Shuffles the queue. */
-	public shuffle() {
+	public shuffle(): void {
 		for (let i = this.length - 1; i > 0; i -= 1) {
 			const j = Math.floor(Math.random() * (i + 1));
 			[this[i], this[j]] = [this[j], this[i]];
