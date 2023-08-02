f315493122d7004755220311b49819d9c4bc5f2e
scrappie <scr3ppie@gmail.com>
2023-08-03 02:38:07 +0300

    refactor(utils): consolidate validation checks

--- src/Utils/ValidateOptions.ts
+++ src/Utils/ValidateOptions.ts
@@ -15,52 +15,29 @@ export function validateOptions(options: NodeOptions, secondaryOptions: Automata
 	)
 		throw new TypeError('Node option "host" must be present and be a non-empty string.');
 
-	if (
-		typeof options.port !== 'undefined' &&
-      typeof options.port !== 'number'
-	)
+	if (typeof options.port !== 'number')
 		throw new TypeError('Node option "port" must be a number.');
 
-	if (
-		typeof options.password !== 'undefined' &&
-      (typeof options.password !== 'string' ||
+	if ((typeof options.password !== 'string' ||
       !/.+/.test(options.password))
 	)
 		throw new TypeError('Node option "password" must be a non-empty string.');
 
-	if (
-		typeof options.secure !== 'undefined' &&
-      typeof options.secure !== 'boolean'
-	)
+	if (typeof options.secure !== 'boolean')
 		throw new TypeError('Node option "secure" must be a boolean.');
 
-	if (
-		typeof options.name !== 'undefined' &&
-      typeof options.name !== 'string'
-	)
+	if (typeof options.name !== 'string')
 		throw new TypeError('Node option "name" must be a non-empty string.');
 
-	if (
-		typeof secondaryOptions.resumeStatus !== 'undefined' &&
-		typeof secondaryOptions.resumeStatus !== 'boolean'
-	)
-		throw new TypeError('Manager option "resumeKey" must be a boolean.');
+	if (typeof secondaryOptions.resumeStatus !== 'boolean')
+		throw new TypeError('Manager option "resumeStatus" must be a boolean.');
 
-	if (
-		typeof secondaryOptions.resumeTimeout !== 'undefined' &&
-		typeof secondaryOptions.resumeTimeout !== 'number'
-	)
+	if (typeof secondaryOptions.resumeTimeout !== 'number')
 		throw new TypeError('Manager option "resumeTimeout" must be a number.');
 
-	if (
-		typeof secondaryOptions.reconnectTimeout !== 'undefined' &&
-		typeof secondaryOptions.reconnectTimeout !== 'number'
-	)
+	if (typeof secondaryOptions.reconnectTimeout !== 'number')
 		throw new TypeError('Manager option "reconnectTimeout" must be a number.');
 
-	if (
-		typeof secondaryOptions.reconnectTries !== 'undefined' &&
-		typeof secondaryOptions.reconnectTries !== 'number'
-	)
+	if (typeof secondaryOptions.reconnectTries !== 'number')
 		throw new TypeError('Manager option "reconnectTries" must be a number.');
 }
